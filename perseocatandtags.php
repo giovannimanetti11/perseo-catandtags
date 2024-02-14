a630400b8779a268af651117c1ab056ede164434
giovannimanetti11 <giovanni@perseodesign.com>
2024-02-14 17:05:21 +0000

    Add support for already created tags and categories

--- perseocatandtags.php
+++ perseocatandtags.php
@@ -40,16 +40,6 @@ function perseo_add_taxonomy_image_field() {
 add_action('category_add_form_fields', 'perseo_add_taxonomy_image_field', 10, 2);
 add_action('post_tag_add_form_fields', 'perseo_add_taxonomy_image_field', 10, 2);
 
-// Save the image data when the term is saved
-function perseo_save_taxonomy_image($term_id) {
-	if (isset($_POST['perseo-category-image-id']) && '' !== $_POST['perseo-category-image-id']){
-		$image = $_POST['perseo-category-image-id'];
-		add_term_meta($term_id, 'perseo-category-image-id', $image, true);
-	}
-}
-add_action('created_category', 'perseo_save_taxonomy_image');
-add_action('created_post_tag', 'perseo_save_taxonomy_image');
-
 // Add a column to the term list table to display the image
 function perseo_add_image_column_to_term_list($columns) {
 	$columns['perseo_image'] = __('Image', 'perseo');
@@ -72,12 +62,58 @@ add_filter('manage_category_custom_column', 'perseo_fill_image_column_in_term_li
 add_filter('manage_post_tag_custom_column', 'perseo_fill_image_column_in_term_list', 10, 3);
 
 
-// Add .svg support to WordPress
-function perseo_enable_svg_upload($mimes) {
-    $mimes['svg'] = 'image/svg+xml';
-    return $mimes;
+// Add image field in edit form
+function perseo_edit_taxonomy_image_field($term) {
+    // Ensure the $term is an object
+    if (!is_object($term)) return;
+    
+    $image_id = get_term_meta($term->term_id, 'perseo-category-image-id', true);
+    ?>
+    <tr class="form-field term-group-wrap">
+        <th scope="row">
+            <label for="perseo-category-image-id"><?php _e('Image', 'perseo'); ?></label>
+        </th>
+        <td>
+            <input type="hidden" id="perseo-category-image-id" name="perseo-category-image-id" value="<?php echo esc_attr($image_id); ?>">
+            <div id="category-image-wrapper">
+                <?php if ($image_id) { echo wp_get_attachment_image($image_id, 'thumbnail'); } ?>
+            </div>
+            <p>
+                <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e('Add Image', 'perseo'); ?>" />
+                <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e('Remove Image', 'perseo'); ?>" />
+            </p>
+        </td>
+    </tr>
+    <?php
 }
-add_filter('upload_mimes', 'perseo_enable_svg_upload');
+add_action('category_edit_form_fields', 'perseo_edit_taxonomy_image_field', 10, 2);
+add_action('post_tag_edit_form_fields', 'perseo_edit_taxonomy_image_field', 10, 2);
+
+// Save the image data on term creation and edit
+function perseo_save_taxonomy_image_on_create($term_id) {
+    if (isset($_POST['perseo-category-image-id']) && '' !== $_POST['perseo-category-image-id']){
+        $image = $_POST['perseo-category-image-id'];
+        add_term_meta($term_id, 'perseo-category-image-id', $image, true);
+    }
+}
+add_action('created_category', 'perseo_save_taxonomy_image_on_create');
+add_action('created_post_tag', 'perseo_save_taxonomy_image_on_create');
+
+
+function perseo_save_taxonomy_image_on_edit($term_id) {
+    if (isset($_POST['perseo-category-image-id']) && '' !== $_POST['perseo-category-image-id']){
+        $image = $_POST['perseo-category-image-id'];
+        update_term_meta($term_id, 'perseo-category-image-id', $image);
+    } else {
+        delete_term_meta($term_id, 'perseo-category-image-id');
+    }
+}
+add_action('edited_category', 'perseo_save_taxonomy_image_on_edit', 10, 2);
+add_action('edited_post_tag', 'perseo_save_taxonomy_image_on_edit', 10, 2);
+
+
+
+
 
 
 
